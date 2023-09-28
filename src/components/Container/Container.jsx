1e160063cbb63878f14f0580c5c9b6ea880728fc
hariprasathys22 <confluenceinfotechsolutions@gmail.com>
2023-09-29 01:44:56 +0530

    Completed connecting to weatherAPI

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -11,43 +11,41 @@ const Container = () => {
   const [currentWindSpeed, setCurrentWindSpeed] = useState(null);
   const [prevWindSpeed, setPrevWindSpeed] = useState(null);
   const [rateOfChange, setRateOfChange] = useState(null); // New state to hold rate of change
-
-  const [rainChance, setRainChance] = useState(0)
+  const [ pressure, setPressure] = useState(0)
+  const [humidity, setHumidity] = useState(0)
+  const [temparature, setTemparature] = useState(null)
 
   useEffect(() => {
     const apiKey = "ea4c44a9a8632d9e7c12a667bae5df78";
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=coimbatore&appid=${apiKey}&units=metric`;
 
     axios
-      .get(apiUrl)
-      .then((response) => {
-        const newWindSpeed = response.data.wind.speed;
-
-        if (prevWindSpeed !== null) {
-          // Calculate the rate of change
-          const newRateOfChange = newWindSpeed - prevWindSpeed;
-          setRateOfChange(newRateOfChange); // Set the rate of change state
-          setCurrentWindSpeed(newWindSpeed);
-          setPrevWindSpeed(prevWindSpeed);
-        } else {
-          setCurrentWindSpeed(newWindSpeed);
-          setPrevWindSpeed(newWindSpeed);
-        }
-
-        setWeatherData(response.data);
-        setLoading(false);
-        if (response.data.list[0].rain) {
-          setRainChance(response.data.list[0].rain["3h"]); // Get the 3-hour rain volume
-        }
-
-
-
-
-      })
-      .catch((err) => {
-        setError(err);
-        setLoading(false);
-      });
+    .get(apiUrl)
+    .then((response) => {
+      const newWindSpeed = response.data.wind.speed;
+  
+      if (prevWindSpeed !== null) {
+        // Calculate the rate of change
+        const newRateOfChange = newWindSpeed - prevWindSpeed;
+        setRateOfChange(newRateOfChange); // Set the rate of change state
+      }
+  
+      setCurrentWindSpeed(newWindSpeed);
+      setPrevWindSpeed(newWindSpeed);
+      setWeatherData(response.data);
+      setLoading(false);
+      setHumidity(response.data.main.humidity)
+      setPressure(response.data.main.pressure)
+      setTemparature(response.data.main.temp)
+      // Debugging: Log the entire response data
+      console.log(response.data);
+  
+    
+    })
+    .catch((err) => {
+      setError(err);
+      setLoading(false);
+    });
   }, [prevWindSpeed]);
 
   if (loading) {
@@ -62,7 +60,7 @@ const Container = () => {
     <div className="container">
       <TopContainer />
       <hr size="1" />
-      <Info data={weatherData} currentWindSpeed={currentWindSpeed} rateOfChange={rateOfChange} rainChance={rainChance}/> {/* Pass rateOfChange as a prop */}
+      <Info data={weatherData} currentWindSpeed={currentWindSpeed} rateOfChange={rateOfChange} humidity={humidity} pressures={pressure}  temparature = {temparature} /> {/* Pass rateOfChange as a prop */}
     </div>
   );
 };
--- src/components/Container/MidContent/Info.jsx
+++ src/components/Container/MidContent/Info.jsx
@@ -4,7 +4,7 @@ import { AiOutlineCaretDown, AiOutlineCaretUp } from "react-icons/ai";
 import { BsCloudHaze2 } from "react-icons/bs";
 import "./Info.css";
 
-const Info = ({data, rateOfChange, currentWindSpeed, rainChance}) => {
+const Info = ({currentWindSpeed, humidity, pressures, temparature}) => {
   return (
     <div className="info-container">
       <div className="info-head">
@@ -29,13 +29,7 @@ const Info = ({data, rateOfChange, currentWindSpeed, rainChance}) => {
               <h2 className="speed-headkm-head">{currentWindSpeed}km/h</h2>
             </div>
           </div>
-          <div className="speed-flow">
-            <AiOutlineCaretDown className="ios1" />
-
-           {rateOfChange !== null && (
-               <p className="speed-flow-hr">{rateOfChange}</p>
-            )}
-          </div>
+          
         </div>
         <div className="wind-speed-we">
           <div className="info-logo">
@@ -43,17 +37,13 @@ const Info = ({data, rateOfChange, currentWindSpeed, rainChance}) => {
           </div>
           <div className="wind-speed-meter">
             <div className="speed-text">
-              <p className="speed-para">Rain Chanse</p>
+              <p className="speed-para">Humidity</p>
             </div>
             <div className="speed-km">
-              <h2 className="speed-headkm-head">{rainChance}%</h2>
+              <h2 className="speed-headkm-head">{humidity}%</h2>
             </div>
           </div>
-          <div className="speed-flow">
-            <AiOutlineCaretDown className="ios1" />
-
-            <p className="speed-flow-hr">10%</p>
-          </div>
+         
         </div>
         <div className="wind-speed-we">
           <div className="info-logo">
@@ -64,14 +54,10 @@ const Info = ({data, rateOfChange, currentWindSpeed, rainChance}) => {
               <p className="speed-para">Pressure</p>
             </div>
             <div className="speed-km">
-              <h2 className="speed-headkm-head">720 hpa</h2>
+              <h2 className="speed-headkm-head">{pressures}hpa</h2>
             </div>
           </div>
-          <div className="speed-flow">
-            <AiOutlineCaretDown className="ios1" />
-
-            <p className="speed-flow-hr">32 hpa</p>
-          </div>
+          
         </div>
         <div className="wind-speed-we">
           <div className="info-logo">
@@ -82,14 +68,10 @@ const Info = ({data, rateOfChange, currentWindSpeed, rainChance}) => {
               <p className="speed-para">Uv Index</p>
             </div>
             <div className="speed-km">
-              <h2 className="speed-headkm-head">2,3</h2>
+              <h2 className="speed-headkm-head">{temparature}°C</h2>
             </div>
           </div>
-          <div className="speed-flow">
-            <AiOutlineCaretDown className="ios1" />
-
-            <p className="speed-flow-hr">0,3</p>
-          </div>
+          
         </div>
       </div>
     </div>
